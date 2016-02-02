1f2ad82aeb582f8762b5ed72f1acea655da89683
Stijn Elskens <stijn@statik.be>
2016-02-02 09:21:58 +0100

    added scroll to top function

--- js/components/kul.js
+++ js/components/kul.js
@@ -147,3 +147,22 @@ window.debounce = function(func, wait, immediate) {
     if (callNow) func.apply(context, args);
   };
 };
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 400) {
+            $('.scroll-to-top').fadeIn();
+        } else {
+            $('.scroll-to-top').fadeOut();
+        }
+    });
+
+    $('.scroll-to-top').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+
+});
